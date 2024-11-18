3c29eff6fac3e45110e954c523f12e5320e6bb45
septchou <thichanonsittisomboon@gmail.com>
2024-11-18 17:13:57 +0000

    add

--- calculator.py
+++ calculator.py
@@ -3,24 +3,42 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
-
+        return a - b
+    
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        c=a
+        d=b
+        if c< 0:
+            a = 0-a
+        if d< 0:
+            b = 0-b
+        for i in range(b):
             result = self.add(result, a)
+        if (c<0 and d>0) or (c>0 and d<0):
+            result = 0-result
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        c=a
+        d=b
+        if c< 0:
+            a = 0-a
+        if d< 0:
+            b = 0-b
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        if (c<0 and d>0) or (c>0 and d<0):
+            result = 0-result
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+
+        while a >= b:
             a = a-b
+        
         return a
 
 # Example usage:
@@ -28,7 +46,7 @@ if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
     print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: subtraction: ", calc.subtract(-1, -2))
+    print("Example: multiplication: ", calc.multiply(2, -3))
+    print("Example: division: ", calc.divide(10, -8))
+    print("Example: modulo: ", calc.modulo(1, 2))
--- test_calculator.py
+++ test_calculator.py
@@ -6,8 +6,22 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+    def test(self):
+        self.assertEqual(self.calc.add(1, -2), -1)
+        self.assertEqual(self.calc.add(-1, -2), -3)
+
+        self.assertEqual(self.calc.subtract(1, -2), 3)
+        self.assertEqual(self.calc.subtract(-1, -2), 1)
+
+        self.assertEqual(self.calc.multiply(-1, -2), 2)
+        self.assertEqual(self.calc.multiply(2, -3), -6)
+
+        self.assertEqual(self.calc.divide(1, -2), 0)
+        self.assertEqual(self.calc.divide(-10, -8), 1)
+
+        self.assertEqual(self.calc.modulo(1, 2), 1)
+        self.assertEqual(self.calc.modulo(8, 2), 0)
+
 
     # Add the following test methods to the TestCalculator class:
 
